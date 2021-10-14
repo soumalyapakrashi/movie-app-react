7206da33653c4ef631a2ed3b1ad40da685668544
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2021-10-14 17:07:45 +0530

    Finish movie details page

---
+++ src/components/Actor/Actor.styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    color: var(--white);
+    background: var(--darkGray);
+    border-radius: 20px;
+    padding: 5px;
+    text-align: center;
+
+    h3 {
+        margin: 10px 0px 0px 0px;
+    }
+
+    p {
+        margin: 5px 0px;
+    }
+`;
+
+export const Image = styled.img`
+    display: block;
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 15px;
+`;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,6 +8,9 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 import BreadCrumb from "./BreadCrumb";
+import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 
 // Import Image
 import NoImage from '../images/no_image.jpg';
@@ -25,6 +28,22 @@ function Movie() {
     return(
         <>
             <BreadCrumb movieTitle = { movie.original_title } />
+            <MovieInfo movie = { movie } />
+            <MovieInfoBar 
+                time = { movie.runtime } 
+                budget = { movie.budget } 
+                revenue = { movie.revenue } 
+            />
+            <Grid header = "Actors">
+                { movie.actors.map(actor => (
+                    <Actor 
+                        key = { actor.credit_id }
+                        name = { actor.name }
+                        character = { actor.character }
+                        imageUrl = { actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage }
+                    />
+                ))}
+            </Grid>
         </>
     );
 }
